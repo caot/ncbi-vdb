2ece980b892cdace67c87689b2b00861784af9c4
Andrew Klymenko <klymenka@nih.gov>
2019-03-06 15:04:51 -0500

    testing https://sponomar-test.ncbi.nlm.nih.gov/Traces/names/names.fcgi

--- test/vfs/resolver-cgi.h
+++ test/vfs/resolver-cgi.h
@@ -1,1 +1,3 @@
-#define RESOLVER_CGI "https://trace.ncbi.nlm.nih.gov/Traces/names/names.fcgi"
+/*
+#define RESOLVER_CGI "https://trace.ncbi.nlm.nih.gov/Traces/names/names.fcgi"*/
+#define RESOLVER_CGI "https://sponomar-test.ncbi.nlm.nih.gov/Traces/names/names.fcgi"
